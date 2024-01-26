2549fdca746bbb6c6e8b2ad2c7ff74ccc9bc2406
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2024-01-27 03:01:21 +0530

    added register and login and sending cooies in every request done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm ">
       <div className="py-4 border-b-[1px] ">
-      {/* <button onClick={handleCLick}>Click me</button> */}
+      <button onClick={handleCLick}>Click me</button>
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             {/* logo */}
